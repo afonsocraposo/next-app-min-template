1a3b14115c76a1a18af7494a86a2b7bb2862bd2c
Afonso Raposo <afonso.raposo@paddle.com>
2025-12-22 11:43:37 +0000

    fix: user repository

--- src/lib/repositories/user.ts
+++ src/lib/repositories/user.ts
@@ -1,16 +1,16 @@
 import { User } from "@/lib/db/types";
 import { db } from "@/lib/db";
-import { user } from "@/lib/db/auth-schema";
+import { users } from "@/lib/db/auth-schema";
 import { eq } from "drizzle-orm";
 
 export class UserRepository {
-  constructor(public readonly id: string) { }
+  constructor(public readonly id: string) {}
 
   async getUser(): Promise<User | null> {
     const [dbUser] = await db
       .select()
-      .from(user)
-      .where(eq(user.id, this.id))
+      .from(users)
+      .where(eq(users.id, this.id))
       .limit(1);
     return dbUser || null;
   }
